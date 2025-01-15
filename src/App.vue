6fde0f4e5ae7ebf5969abb384339221a14dbec63
Lukyss (Quentin Lachery) <quentin.wow@free.fr>
2025-01-15 16:54:19 +0100

    implémentation tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1 class="text-8xl font-bold underline">Oy</h1>
 
     <div class="wrapper">
 
