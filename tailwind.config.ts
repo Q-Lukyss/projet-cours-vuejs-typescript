017af5ab43b966bef9e31c1a904d576941ce6751
Q-Lukyss <quentin.wow@free.fr>
2025-02-10 14:50:24 +0100

    color dans extends et pas dans theme sinon ça sucre les couleurs de base

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,15 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    colors:{
-      "darkblue": "#1F1A38",
-      "violet": "#7B506F",
-      "lightviolet": "#DD99BB",
-      "beige": "#DBCDC6",
-      "lightbeige": "#EAD7D1",
+    extend: {
+      colors:{
+        "darkblue": "#1F1A38",
+        "violet": "#7B506F",
+        "lightviolet": "#DD99BB",
+        "beige": "#DBCDC6",
+        "lightbeige": "#EAD7D1",
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
